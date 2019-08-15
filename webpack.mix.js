5635177adc0848349e1f4dd9f4994b59f1730cda
Pujen Solanki <6865299+Solankill@users.noreply.github.com>
2019-08-15 07:57:01 -0700

    win
    win

--- webpack.mix.js
+++
@@ -1,8 +0,0 @@
-let mix = require('laravel-mix');
-let tailwindcss = require('tailwindcss');
-mix.js('resources/js/app.js', 'public/js')
-.sass('resources/sass/app.scss', 'public/css')
-.options({
-    postCss: [ tailwindcss('./tailwind.js') ],
-    processCssUrls: false, // This is required when using laravel mix
-});
