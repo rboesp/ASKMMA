57b677b09bbc72f5cdb2eb3e4bd6c8a12a59ee92
Pujen Solanki <6865299+Solankill@users.noreply.github.com>
2019-08-15 08:05:21 -0700

    Revert "win"
    This reverts commit 5635177adc0848349e1f4dd9f4994b59f1730cda.

---
+++ resources/views/sections/longhaul.blade.php
@@ -0,0 +1,95 @@
+<section class="longhaul py-16">
+
+
+
+<div class="container mx-auto">
+ 
+
+<!--flex-col-reverse lg:flex-row-reverse -->
+	<div class="flex flex-row flexbox flex-wrap ">
+		<!-- Left -->
+
+		<div class="card-left flex flex-col lg:w-1/2 lg:pr-8">
+
+			<img src="/images/longhaul.png">
+
+		</div>
+
+
+		 
+
+		<div class="card-right flex flex-col lg:w-1/2 lg:pl-8 py-6 px-8 justify-between">
+			
+
+			<div class="flex flex-row">
+
+				<h3 class="text-3xl lg:text-4xl text-center lg:text-left font-black w-full leading-none lg:leading-normal">Around for the <br class="lg:hidden">Long Haul</h3>
+				
+			</div>
+			
+
+			<div class="flex flex-row mt-6 lg:mt-0">
+
+				<p class="lg:text-base text-lg w-full text-center lg:text-left">Separated they live in Bookmarks right at the coast of the famous Semantics, large language ocean Separated they live in Bookmarks right at the coast.</p>
+				
+			</div>
+
+			<div class="flex flex-row flex-wrap ">
+
+				<div class="flex flex-col flex-wrap w-full lg:w-1/2 p-8 lg:p-0">
+
+					<div class="w-full text-xl lg:text-base text-center lg:text-left font-black">
+						  
+						Feature 1
+						
+					</div>
+
+					<div class="lg:text-base text-l text-center lg:text-left mt-1">
+
+						Gorgeous, high-quality video sharing on desktop, mobile, tablet
+						
+					</div>
+					
+				</div>
+				<div class="flex flex-col flex-wrap w-full lg:w-1/2">
+
+					<div class="w-full text-xl lg:text-base text-center lg:text-left font-black">
+
+						Feature 2
+						
+					</div>
+
+					<div class="lg:text-base text-l text-center lg:text-left mt-1">
+
+						Gorgeous, high-quality video sharing on desktop, mobile, tablet
+						
+					</div>
+					
+				</div>
+				
+
+			</div>
+
+			<div class="flex flex-row justify-center mt-6 lg:mt-0 lg:justify-start">
+
+				<button class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full lg:w-1/4">
+				  Get Coverage
+				</button>
+
+			</div>
+
+
+
+
+		</div>
+
+	</div>
+
+
+	<!-- End Card -->
+
+</div>
+
+
+
+</section>
