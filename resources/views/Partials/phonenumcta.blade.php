eeaf47ed9727383c89fedccb462a4723c3701810
Pujen Solanki <pujen@eliinova.com>
2019-08-27 14:16:14 -0700

    change the cta text

--- resources/views/Partials/phonenumcta.blade.php
+++ resources/views/Partials/phonenumcta.blade.php
@@ -2,9 +2,9 @@
     <div class="container mx-auto">
         <div class="flex flex-row justify-start space-between flex-wrap">
             <div class="flex flex-col w-full lg:w-1/2 text-3xl lg:text-4xl lg:pl-0 px-8 font-bold font-futura text-mmalightblue leading-tight text-center lg:text-left justify-center">
-                Do you have Questions or
+                Need to find plans in your area? 
                 <br class="hidden lg:inline">
-                need Help? Call toll free!
+                Contact a representative now.
             </div>
 
             <div class="flex flex-col w-full lg:w-1/2 flex-wrap pt-6 lg:pt-2 lg:pl-8 text-4xl lg:text-5xl text-white justify-center font-futura font-bold items-center lg:items-end">
