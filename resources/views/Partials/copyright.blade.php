2d7ab65a4445e9cad5f6aa3827e85e853569b9bb
Pujen Solanki <pujen@eliinova.com>
2019-09-03 09:43:02 -0700

    more footer changes

--- resources/views/Partials/copyright.blade.php
+++ resources/views/Partials/copyright.blade.php
@@ -1,7 +1,7 @@
 <section class="copyright bg-mmablue py-6">
 	<div class="container mx-auto">
 		<div class="flex flex-col lg:flex-row flex-wrap text-gray-100">
-			<div class="flex lg:w-1/2 lg:pl-3 justify-center lg:justify-start">© 2019 Medicare Medicaid Advisors. All rights reserved.</div>
+			<div class="flex lg:w-1/2 lg:pl-3 justify-center lg:justify-start">© 2019 Medicare Medicaid Advisors. All Rights Reserved.</div>
 			<div class="flex justify-between mx-4 lg:mx-0 lg:w-1/2 lg:justify-end lg:pr-3 lg:mx-0 text-gray-100 py-4 lg:py-0">
 				<a class="pr-8" href="#">Privacy Policy</a>
 				<a href="#">Terms of Service</a>
