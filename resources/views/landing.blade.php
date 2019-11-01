fcdb26618ac07b01f8580cd918a6507c25571582
Chris Breuer <chrisbreuer93@gmail.com>
2019-11-01 16:17:07 -0700

    add ga tag to home page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -48,7 +48,22 @@
 				transform: rotate(360deg);
 			}
 		}
-	</style>
+    </style>
+
+    @if(config('app.env') == 'production')
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138115450-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+
+            gtag('config', 'UA-138115450-1');
+        </script>
+    @endif
 </head>
 <body class="font-body">
 	@include('partials.nav')
