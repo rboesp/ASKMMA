bf16c151684c5ddbca4a811438be90f27dd621dd
Chris Breuer <chrisbreuer93@gmail.com>
2019-11-19 12:07:49 -0800

    add phone to $fillable

--- app/Models/ContactMessage.php
+++ app/Models/ContactMessage.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class ContactMessage extends Model
 {
-    protected $fillable = ['first_name', 'last_name', 'email', 'zip_code', 'interest', 'receive_newsletter'];
+    protected $fillable = ['first_name', 'last_name', 'email', 'zip_code', 'interest', 'receive_newsletter', 'phone'];
 }
