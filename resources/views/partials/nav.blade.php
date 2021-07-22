66eecba0a03a29cf9d0e6b701e1ff4d6fdaece4d
BobZordel <55206498+BobZordel@users.noreply.github.com>
2021-07-22 10:38:16 -0500

    Logo Style

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -11,7 +11,7 @@
         </div>
         <div class="flex justify-center lg:justify-start text-white lg:mr-6 lg:pl-8 lg:mx-0 w-full mx-auto w-3/4">
             <a href="/" class="flex w-full sm:w-2/3">
-                <img class="h-12 md:h-16" src="images/mmacombo_new.png" />
+                <img class="h-12 md:h-16 header-logo" src="images/mmacombo_new.png" />
             </a>
         </div>
     </div>
