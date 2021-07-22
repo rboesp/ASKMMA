a8b2fb06f92b2412bb77563d704fbef3aed9b812
BobZordel <55206498+BobZordel@users.noreply.github.com>
2021-07-22 10:10:42 -0500

    Better Quality New logo

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -11,7 +11,7 @@
         </div>
         <div class="flex justify-center lg:justify-start text-white lg:mr-6 lg:pl-8 lg:mx-0 w-full mx-auto w-3/4">
             <a href="/" class="flex w-full sm:w-2/3">
-                <img class="h-12 md:h-16" src="images/MMAUSA_newlogo.jpg" />
+                <img class="h-12 md:h-16" src="images/MMACombinedLogo.png" />
             </a>
         </div>
     </div>
