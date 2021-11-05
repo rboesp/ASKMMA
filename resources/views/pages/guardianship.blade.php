3c280f0a2025f9a1ea783e6674d4ee6d703b9c38
Chris <chrisbreuer93@gmail.com>
2021-11-04 18:28:08 -0700

    chore: adjust the video source

--- resources/views/pages/guardianship.blade.php
+++ resources/views/pages/guardianship.blade.php
@@ -18,20 +18,9 @@
                 <div class="card-right flex flex-col w-full lg:w-4/5 mx-auto justify-between">
                     <div class="flex flex-row flex-wrap px-2">
 
-                        <div class="w-full bg-white rounded px-8">
-                            <div class="w-full flex flex-wrap justify-between cursor-pointer">
-                                <h3 class="w-full font-extrabold text-2xl text-center md:text-left pt-8 md:w-5/6 text-gray-800 pt-4">
-                                    Integrity Pharmacy
-                                </h3>
-                                <span class="text-base text-mmablue font-semibold w-full md:w-1/6 text-right">
-                                  <img src="/images/resources/integrity_logo.jpg" class="w-1/2 md:w-4/5 h-auto mx-auto">
-                                </span>
-                            </div>
-                            <div class="w-full border-mmablue text-sm lg:text-base pb-6 flex flex-wrap">
-                                <video controls>
-                                    <source src="/videos/Guardian Testimonial Video.mp4" type="video/mp4">
-                                    Your browser does not support the video tag.
-                                </video>
+                        <div class="w-full bg-white rounded p-8">
+                            <div class="w-full border-mmablue text-sm lg:text-base flex flex-wrap">
+                                <iframe class="h-150 w-full" src="https://www.youtube.com/embed/4jiq2Cb0r7A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             </div>
                         </div>
                     </div>
